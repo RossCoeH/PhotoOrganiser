93553ba5ecc2670532891404371a34da03f1ef43
madbiker88 <madbiker88@gmail.com>
2022-03-22 21:09:41 +1300

    Add project files.

---
+++ PhotoOrganiser/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace PhotoOrganiser
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+    }
+}
